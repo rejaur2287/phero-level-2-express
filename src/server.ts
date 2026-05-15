bbdcd200319277ca749a5a394adb28258745e859
rejaur2287 <rezaurewubd@gmail.com>
2026-05-15 09:58:18 +0600

    Executing Pool and creating tables

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,28 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_fdEaj9MQW7vt@ep-blue-credit-aqke4xgv-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `);
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   // res.send("Hello World!!!");
   res.status(200).json({
