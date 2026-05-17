23bf6af67a99a584f22cf14897598a139561dc3c
rejaur2287 <rezaurewubd@gmail.com>
2026-05-17 22:59:34 +0600

    Creating User Profiles

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import { pool } from "./db";
+
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 const app: Application = express();
 
 app.use(express.json());
@@ -20,5 +21,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
 
 export default app;
